31b14a834ad69a52f1fd9dc5f3cccfe85551721d
nieshiming <shiming.nie@aihuishou.com>
2020-10-30 18:03:08 +0800

    chore: modify

--- src/routers/index.js
+++ src/routers/index.js
@@ -5,11 +5,7 @@ const router = new Router()
 
 module.exports = (app) => {
   router.get('/testget', login)
-  router.get('/nsm', async (ctx) => (ctx.body = '11111111'))
   router.post('/testpost', testpost)
-  router.all('/', async (ctx, next) => {
-    ctx.body = '<h1>404 not found</h1>'
-  })
 
   app.use(router.routes()).use(router.allowedMethods())
 
