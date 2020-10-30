df8fa8fdf3d88f055138be3fc6be6ab60670b870
nieshiming <shiming.nie@aihuishou.com>
2020-10-30 16:59:40 +0800

    chore: add 404

--- src/routers/index.js
+++ src/routers/index.js
@@ -6,6 +6,9 @@ const router = new Router()
 module.exports = (app) => {
   router.get('/testget', login)
   router.post('/testpost', testpost)
+  router.all('/', async (ctx, next) => {
+    ctx.body = '<h1>404 not found</h1>'
+  })
 
   app.use(router.routes()).use(router.allowedMethods())
 
