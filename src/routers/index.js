bd9434d57d138002d53256bae0bb7e2cdd92145e
nieshiming <shiming.nie@aihuishou.com>
2020-11-02 17:08:12 +0800

    chore: add prettier

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,24 +1,24 @@
-const Router = require('koa-router')
-const { login, testpost, getJson } = require('../controller')
+const Router = require('koa-router');
+const { login, testpost, getJson } = require('../controller');
 
 const router = new Router({
   prefix: '/levi',
-})
+});
 
 module.exports = (app) => {
   router.get('/testget', login, async (ctx, next) => {
     // console.log(ctx.tagName)
-    await next()
-  })
-  router.post('/testpost', testpost)
-  router.get('/getJson', getJson)
+    await next();
+  });
+  router.post('/testpost', testpost);
+  router.get('/getJson', getJson);
   router.get('/404', async (ctx, next) => {
-    ctx.body = ''
-    ctx.response.status = 404
-    await next()
-  })
+    ctx.body = '';
+    ctx.response.status = 404;
+    await next();
+  });
 
-  app.use(router.routes()).use(router.allowedMethods())
+  app.use(router.routes()).use(router.allowedMethods());
 
-  return router
-}
+  return router;
+};
--- src/server.js
+++ src/server.js
@@ -1,23 +1,23 @@
-const Koa = require('koa')
-const bodyParser = require('koa-bodyparser')
-const router = require('./routers')
-const middleware = require('./middleware')
+const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
+const router = require('./routers');
+const middleware = require('./middleware');
 
-const app = new Koa()
+const app = new Koa();
 
 /** 保证middleware 在router之前加载，因为 app.use有注册顺序，先注册先执行 */
-middleware(app)
-router(app)
-app.use(bodyParser())
+middleware(app);
+router(app);
+app.use(bodyParser());
 
 app.on('error', (err) => {
-  console.log(err)
-})
+  console.log(err);
+});
 
 /** await 中间件执行顺序, 如果上一个中间件没有 加 await next() 那么中间件就不会执行 */
 app.use(async (ctx, next) => {
-  console.log('app use next start')
-  await next()
-})
+  console.log('app use next start');
+  await next();
+});
 
-app.listen(8888, () => console.log('server is start'))
+app.listen(8888, () => console.log('server is start'));
