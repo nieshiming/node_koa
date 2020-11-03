ed4863cfe2e4f737487157604306394863738b0a
nieshiming <shiming.nie@aihuishou.com>
2020-11-03 15:56:31 +0800

    chore: add 引用别名 && tsconfig-paths

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,6 @@ const app = new Koa();
 /** 保证middleware 在router之前加载，因为 app.use有注册顺序，先注册先执行 */
 middleware(app);
 router(app);
-console.log(router);
 app.use(bodyParser());
 
 app.on('error', (err) => {
